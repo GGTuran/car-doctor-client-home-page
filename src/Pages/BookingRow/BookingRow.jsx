fb948b97b5fd03c26902677c056a8c4080acadf7
GGTuran <tayyabturan67@gmail.com>
2024-04-09 00:41:10 +0600

    displaying data in tabular format

---
+++ src/Pages/BookingRow/BookingRow.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+const BookingRow = ({booking}) => {
+
+    const {date, service, price, img} = booking;
+
+  return (
+    <tr>
+      <th>
+        <label>
+          <input type="checkbox" className="checkbox" />
+        </label>
+      </th>
+      <td>
+        
+          <div className="avatar">
+            <div className="rounded w-24 h-24">
+              <img
+                src={img}
+                alt="Avatar Tailwind CSS Component"
+              />
+            </div>
+          </div>
+      
+        
+      </td>
+      <td>
+       {service}
+      </td>
+      <td>${price}</td>
+      <td>${date}</td>
+      <th>
+        <button className="btn btn-ghost btn-xs">details</button>
+      </th>
+    </tr>
+  );
+};
+
+export default BookingRow;
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,26 +1,52 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
-
+import BookingRow from "../BookingRow/BookingRow";
 
 const Bookings = () => {
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
 
-    const { user } = useContext(AuthContext);
-    const[bookings, setBookings] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/bookings?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+      });
+  }, []);
 
-
-    useEffect(() =>{
-        fetch(`http://localhost:5000/bookings?email=${user.email}`)
-        .then(res =>res.json())
-        .then(data =>{
-            console.log(data)
-        })
-    } ,[])
-
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>{bookings.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service Name</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                bookings.map(booking => <BookingRow
+                key={booking._id}
+                booking={booking}
+                ></BookingRow>)
+            }
+          
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Bookings;
