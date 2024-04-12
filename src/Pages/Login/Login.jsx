92b7247acade1cf20699b9992c444ebc8f47491f
GGTuran <tayyabturan67@gmail.com>
2024-04-12 19:09:04 +0600

    auth redirect

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
 
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location);
   const { signIn } = useContext(AuthContext);
 
     const handleLogin = e =>{
@@ -19,6 +22,7 @@ const Login = () => {
         .then((result) => {
           const user = result.user;
           console.log(user);
+          navigate(location?.state ? location?.state : '/')
         }).catch((err) => {
           console.log(err);
         });
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({children}) => {
 
     const { user, loading} = useContext(AuthContext);
+    const location = useLocation();
+    // console.log(location);
 
     if(loading){
         return <progress className="progress w-56"></progress>
@@ -16,7 +18,7 @@ const PrivateRoute = ({children}) => {
     }
 
     return (
-        <Navigate to="/login"></Navigate>
+        <Navigate state={location.pathname} to="/login"></Navigate>
     );
 };
 
